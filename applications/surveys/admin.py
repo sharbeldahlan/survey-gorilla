32529f5940260be7a2f0a7259c318d40c281496a
sharbeldahlan <sharbel.dahlan@gmail.com>
2025-05-28 23:38:53 +0300

    Add tabular view for Conversation model in the admin page
    - View all the fields except for the question_text, since
        it repeats.
    
    - Enable searching for specific values in fields, like
        answer_text or favorite_foods.
    
    - Enable filtering rows by diet_type or by created_at.
    
    - Order by newest rows first.

--- applications/surveys/admin.py
+++ applications/surveys/admin.py
@@ -4,4 +4,10 @@ from django.contrib import admin
 from applications.surveys.models import Conversation
 
 
-admin.site.register(Conversation)
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    """ Table view for Conversation model in the admin page. """
+    list_display = ("id", "answer_text", "favorite_foods", "diet_type", "created_at")
+    search_fields = ("answer_text", "favorite_foods")
+    list_filter = ("diet_type", "created_at")
+    ordering = ("-created_at",)
